1bcc93a60c594033af302d150beb6b70ac47a881
nomadic4life <orbison.dennis@gmail.com>
2019-07-10 08:47:08 -0700

    the beginning of our game

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Frank", room["outside"])
 
 # Write a loop that:
 #
@@ -49,3 +51,8 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+while True:
+    print(
+        f"Greetings {player.name}! Welcome to the {player.current_room.name}, {player.current_room.description}")
+    user_input = input("press something. no idea what though:\n")
+    print(user_input)
